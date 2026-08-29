e12276bad4ed31e5fbf1febf1d0142fbc2bb4cb0
SithilSemitha <sithilsemithad@gmail.com>
2026-08-29 09:02:20 +0530

    feat: Added the initial Commit rendering the spring project

---
+++ src/main/java/com/example/agrifleettask5/Agrifleettask5Application.java
@@ -0,0 +1,13 @@
+package com.example.agrifleettask5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agrifleettask5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agrifleettask5Application.class, args);
+	}
+
+}
